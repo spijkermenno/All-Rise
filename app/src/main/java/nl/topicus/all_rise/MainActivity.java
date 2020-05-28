40786b283dff2a1fc4f9a091196ee3df949d0ac6
RobinvdBerg <428689@student.saxion.nl>
2020-05-28 14:06:01 +0200

    Added functionality motion detection

--- app/src/main/java/nl/topicus/all_rise/MainActivity.java
+++ app/src/main/java/nl/topicus/all_rise/MainActivity.java
@@ -2,13 +2,55 @@ package nl.topicus.all_rise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SensorManager sensorManager;
+    private double prevMagn = 0;
+    boolean moving = false;
+    TextView tvValMotion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        tvValMotion = findViewById(R.id.tv_valMotion);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        SensorEventListener stepDetector = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                if (event != null){
+                    float x_acceleration = event.values[0];
+                    float y_acceleration = event.values[1];
+                    float z_acceleration = event.values[2];
+
+                    double magnitude = Math.sqrt(x_acceleration*x_acceleration*y_acceleration*y_acceleration*z_acceleration*z_acceleration);
+                    double deltaMagn = magnitude - prevMagn;
+                    prevMagn = magnitude;
+
+                    moving = deltaMagn > 6;
+                    tvValMotion.setText("" + moving);
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+
+        sensorManager.registerListener(stepDetector, sensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 }
