9e7e2d21f0a26da0c05488417a3f13b635d438b7
Menno Spijker <482845@student.saxion.nl>
2020-06-08 16:29:24 +0200

    removed top bar.

--- app/src/main/java/nl/topicus/all_rise/activity/MainActivity.java
+++ app/src/main/java/nl/topicus/all_rise/activity/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
         context = MainActivity.this;
