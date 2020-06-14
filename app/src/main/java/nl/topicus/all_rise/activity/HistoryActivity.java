28905e3077954827c58e88af414defabb5a0a77c
RobinvdBerg <428689@student.saxion.nl>
2020-06-14 19:30:04 +0200

    Created activity, intent and noted solution for layout

---
+++ app/src/main/java/nl/topicus/all_rise/activity/HistoryActivity.java
@@ -0,0 +1,19 @@
+package nl.topicus.all_rise.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import nl.topicus.all_rise.R;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+        // Oplossing voor layout
+        //TODO 2 adapters. 1 Voor datums, 1 voor oefeningen onder datum
+    }
+}
